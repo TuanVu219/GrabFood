a40362d11652d62703a7792c665c305795cccec2
TuanVu219 <supersaza219@gmail.com>
2025-03-03 15:57:07 +0700

    DeleteCartItem

--- backend/GrabFood/urls.py
+++ backend/GrabFood/urls.py
@@ -7,7 +7,7 @@ from .views import (
     Restaurant_Retrieve, RestaurantList, AddFoodType, FoodTypeList, 
     FoodType_Retrieve, AddMenu, MenuList, Menu_Retrieve, AddReviewMenu, 
     ListReviewMenu, ReviewMenu_Retrieve, RegisterShipper, Shipper_Retrieve, 
-    AddCart, SearchCart,AddFavouriteMenu,ListFavouriteMenu,DeleteFavouriteMenu,AddCartItem,CartItem_List
+    AddCart, SearchCart,AddFavouriteMenu,ListFavouriteMenu,DeleteFavouriteMenu,AddCartItem,CartItem_List,DeleteCartItem
 )
 from rest_framework.authtoken.views import obtain_auth_token
 from django.contrib.auth import views as auth_views
@@ -104,7 +104,8 @@ urlpatterns += [
 #CartItem Urls
 urlpatterns += [
     path('add_cartitem/', AddCartItem.as_view(), name='add_cartitem'),
-    path('list_cartitem/', CartItem_List.as_view(), name='list_cartitem'),
+    path('list_cartitem/<uuid:id_cart>/', CartItem_List.as_view(), name='list_cartitem'),
+    path('delete_cartitem/', DeleteCartItem.as_view(), name='delete_cartitem'),
 
    
 
